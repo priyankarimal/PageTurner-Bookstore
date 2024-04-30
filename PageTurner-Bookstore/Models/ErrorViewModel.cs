a5e5873eb63e84a6ca7d42f2e2e647723435694e
Priyanka <priyankarimal87@gmail.com>
2024-04-30 08:15:52 +0545

    Add project files.

---
+++ PageTurner-Bookstore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PageTurner_Bookstore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
