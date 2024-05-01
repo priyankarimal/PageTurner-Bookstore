97fba7d64c753a15546c426245a9facdc07b63fc
Priyanka <priyankarimal87@gmail.com>
2024-05-01 15:09:23 +0545

    created a controller

---
+++ PageTurner-Bookstore/Controllers/BookController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PageTurner_Bookstore.Controllers
+{
+    public class BookController : Controller
+    {
+        public string GetAllBooks()
+        {
+            return "All books";
+        }
+        public string GetBook(int id)
+        {
+            return $"book with id ={id}";
+        }
+        public string SearchBook(string bookName, string authorName)
+        {
+            return $"Book with name={bookName} &Author={authorName}";
+        }
+    }
+}
